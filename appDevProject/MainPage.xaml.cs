1e8d4fee0f0191538a695c711a211daa10cb9234
Kevin Delassus <g00270791@gmit.ie>
2018-02-13 15:08:48 +0000

    Http request working, Able to DeserializeObject and display to user.

--- appDevProject/MainPage.xaml.cs
+++ appDevProject/MainPage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -25,53 +27,30 @@ namespace appDevProject
         public MainPage()
         {
             this.InitializeComponent();
+            getData();
+        }
+
+        async void getData()
+        {
+            string url = "http://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=184&format=json";
+
+            HttpClient client = new HttpClient();
+
+            string response = await client.GetStringAsync(url);
+
+            var data = JsonConvert.DeserializeObject<Rootobject>(response);
+
+            tblResults.Text = data.stopid.ToString();
+
         }
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
             tbxSubmit.Visibility = Visibility.Collapsed;
             btnSubmit.Visibility = Visibility.Collapsed;
+            tblResults.Visibility = Visibility.Visible;
         }
-        private async System.Threading.Tasks.Task getBusDataAsync()
-        {
-            //Create an HTTP client object
-            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
-
-            //Add a user-agent header to the GET request. 
-            var headers = httpClient.DefaultRequestHeaders;
-
-            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
-            //especially if the header value is coming from user input.
-            string header = "ie";
-            if (!headers.UserAgent.TryParseAdd(header))
-            {
-                throw new Exception("Invalid header value: " + header);
-            }
-
-            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
-            if (!headers.UserAgent.TryParseAdd(header))
-            {
-                throw new Exception("Invalid header value: " + header);
-            }
-
-            Uri requestUri = new Uri("http://www.contoso.com");
-
-            //Send the GET request asynchronously and retrieve the response as a string.
-            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
-            string httpResponseBody = "";
-
-            try
-            {
-                //Send the GET request
-                httpResponse = await httpClient.GetAsync(requestUri);
-                httpResponse.EnsureSuccessStatusCode();
-                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-            }
-            catch (Exception ex)
-            {
-                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
-
-            }
-        }
+       
+        
     }
 }
