3c3a217caebca6e6c20f0a518e6229867e4b1a3e
Kevin Delassus <g00270791@gmit.ie>
2018-04-09 09:30:06 +0100

    Added Regions and commented half the code on the FirstTimeUser page

--- appDevProject/FirstTimeUser.xaml.cs
+++ appDevProject/FirstTimeUser.xaml.cs
@@ -29,7 +29,7 @@ namespace UWP_Main_App
     /// </summary>
     public sealed partial class FirstTimeUser : Page
     {
-
+        #region Variables
         private string stopName1;
         private string stopName2;
 
@@ -42,13 +42,13 @@ namespace UWP_Main_App
         private List<Result> galwayStops = new List<Result>();
         ObservableCollection<string> stops = new ObservableCollection<string>();
         private ObservableCollection<String> suggestions;
-
+        
         //Latitude and Longitude for area around Galway City
         private double galLatLow = 53.01347187;
         private double galLatHigh = 53.54880427;
         private double galLongLeft = -9.58028032;
         private double galLongRight = -8.4061165;
-
+        #endregion
         public FirstTimeUser()
         {
             this.InitializeComponent();
@@ -57,39 +57,46 @@ namespace UWP_Main_App
             getBusStops();
         }
 
+        #region getBusStops - A method which makes a get request and retreves all the bus-stop data in the Co. Galway area. 
         async void getBusStops()
         {
 
             try
             {
+                // Adapted from : https://www.youtube.com/watch?v=UMQ2JVOE_xE
+
+                // Url where the get request will be sent too
                 string url = "http://data.dublinked.ie/cgi-bin/rtpi/busstopinformation?&operator=BE&format=json%22";
 
+                //
                 HttpClient client = new HttpClient();
+                // Making get request and storing responce in response2
+                string response = await client.GetStringAsync(url);
 
-                string response2 = await client.GetStringAsync(url);
+                var busData = JsonConvert.DeserializeObject<Rootobject>(response);
 
-                var busData = JsonConvert.DeserializeObject<Rootobject>(response2);
-
-
-                System.Diagnostics.Debug.WriteLine("Inside If..");
                 System.Diagnostics.Debug.WriteLine("Loading Data..");
 
+                // An algorithm to filter throught all the bus stops and store the stops which are in the galway area.
+                // This is done by setting 4 points using Latitude and Longtitude.
                 for (int i = 1; i < busData.numberofresults; i++)
                 {
+                    // If bus stop is within set latitudes
                     if (busData.results[i].latitude < galLatHigh && busData.results[i].latitude > galLatLow)
                     {
+                        // If bus stop is within set logitudes
                         if (busData.results[i].longitude > galLongLeft && busData.results[i].longitude < galLongRight)
                         {
+                            // Creating an instance of MenuFlyoutItem
                             MenuFlyoutItem item = new MenuFlyoutItem();
+                            // Assigning it a name
                             item.Text = busData.results[i].fullname.ToString();
+                            // Assigning it with a click event
                             item.Click += Item_Click1;
-                            flyStops1.Items.Add(item);
 
-                            MenuFlyoutItem item2 = new MenuFlyoutItem();
-                            item2.Text = busData.results[i].fullname.ToString();
-                            item2.Click += Item_Click2;
-                            flyStops2.Items.Add(item2);
-
+                            // Adding item into flyStops1 and flyStops2
+                            flyStops1.Items.Add(item);  
+                            flyStops2.Items.Add(item);
                             galwayStops.Add(busData.results[i]);
                         }
                     }
@@ -98,12 +105,15 @@ namespace UWP_Main_App
             }
             catch
             {
-                MessageDialog message = new MessageDialog("You have no data..");
+                // Displaying message to user if no data is avaible
+                MessageDialog message = new MessageDialog("You have no Internet Data..");
                 await message.ShowAsync();
             }
            
         }
-
+        #endregion
+       
+        #region Click Methods
         private void Item_Click1(object sender, RoutedEventArgs e)
         {
             MenuFlyoutItem click = (MenuFlyoutItem)sender;
@@ -113,7 +123,6 @@ namespace UWP_Main_App
             stop1Sel = true;
             System.Diagnostics.Debug.WriteLine("Stop 1 is: " + stop1Sel);
         }
-
         private void Item_Click2(object sender, RoutedEventArgs e)
         {
             MenuFlyoutItem click = (MenuFlyoutItem)sender;
@@ -123,13 +132,12 @@ namespace UWP_Main_App
             stop2Sel = true;
             System.Diagnostics.Debug.WriteLine("Stop 2 is: " + stop2Sel);
         }
-
         private async void btnSubmit_ClickAsync(object sender, RoutedEventArgs e)
         {
-            
+
             ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
 
-            if(stop1Sel == false ||  stop2Sel == false)
+            if (stop1Sel == false || stop2Sel == false)
             {
                 MessageDialog message = new MessageDialog("Two Bus-stop need to be selected before continuing..");
                 await message.ShowAsync();
@@ -152,8 +160,9 @@ namespace UWP_Main_App
                 Frame.Navigate(typeof(MainPage));
             }
         }
-        
+        #endregion
 
+        #region searchId - A method for searching the stopId for the selected stop name
         private string searchID(string s)
         {
             string id = "";
@@ -167,7 +176,9 @@ namespace UWP_Main_App
             }
             return id;
         }
-      
+        #endregion
+
+        #region OnNavigated Methods
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
@@ -186,18 +197,20 @@ namespace UWP_Main_App
             }
 
         }
-
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
             showWelcomeMessageAsync();        
         }
+        #endregion
 
+        #region ShowWelcomeMessageAsync - When app is first opened a welcome message is shown to user 
         private async System.Threading.Tasks.Task showWelcomeMessageAsync()
         {
             MessageDialog message = new MessageDialog("Welcome to Commute! Please Select two Bus-Stops from the two select boxes on the on the right and Click the Submit Button.");
             await message.ShowAsync();
         }
+        #endregion
     }
 }
